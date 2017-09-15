debd900033aa07af14a20061062b7974db31460c
David Reep <davereep@gmail.com>
2017-09-15 23:10:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-   else {
+   else if (string.toUpperCase() === string) {
      return "YES INDEED!"
    }
   // else if (string.toUpperCase === string) {

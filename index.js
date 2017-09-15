1acdcad7bbac1011a0c6ca5a0d3b07c6ac6850eb
David Reep <davereep@gmail.com>
2017-09-15 23:11:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ function sayHiToGrandma(string) {
    else if (string.toUpperCase() === string) {
      return "YES INDEED!"
    }
+   else if (string === "I love you, Grandma.")
   // else if (string.toUpperCase === string) {
   //   return "YES INDEED!"
   // }

96186c358c7ae894cc766422b4ad7c57710e1028
David Reep <davereep@gmail.com>
2017-09-15 23:01:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+function sayHiToGrandma(string) {
+  expect (sayHiToGrandma(string)).toEqual(string.lowerCase)
+}
